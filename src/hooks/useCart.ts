0a64f3a371a30b81f7ec04332f6a1de0c0b49a62
Kaguenoakuma <shadowfocker666@gmail.com>
2024-12-31 13:18:15 -0600

    Correccion archivo useCart.ts aplicaccion funcionando.

--- src/hooks/useCart.ts
+++ src/hooks/useCart.ts
@@ -76,8 +76,7 @@ export const useCart = () =>
 
     const cartTotal = useMemo( () => cart.reduce( (total, item) => total + (item.quantity * item.price),0 ), [cart]);
 
-    return
-    {
+    return {
         data,
         cart,
         addToCart,
